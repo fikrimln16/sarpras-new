d5d2656b8bf6bf4d09a8c06acf9fe2c241b9c569
Fikri Maulana <maulanafikri455@gmail.com>
2024-05-15 18:49:50 +0700

    tambah fitur sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,43 +1,46 @@
 <div class="sidebar">
     <div class="datetime">{{ Carbon\Carbon::now()->isoFormat('D MMMM YYYY - HH:mm:ss') }}</div>
+    {{-- sarpras unit pt --}}
     @if (auth()->check() && auth()->user()->role === '2')
-    @php
-    $universities = auth()->user()->universities;
-    $universityName = $universities->isNotEmpty()
-    ? $universities->first()->nama_kampus
-    : 'No University Assigned';
-    @endphp
-    <div class="welcome">
-        Selamat datang, {{ Auth::user()->name }}<br>
-        Unit Sarpras PT {{ $universityName }}<br>
-    </div>
-    <div class="menu-item" data-toggle="submenu">Manajemen Aset</div>
-    <div class="submenu">
-        <a href="{{ route('manajemen_aset.prasarana') }}" class="menu-item">Prasarana</a>
-        <a href="{{ route('manajemen_aset.sarana') }}" class="menu-item">Sarana</a>
-        <a href="" class="menu-item">Inventaris</a>
-    </div>
+        @php
+            $universities = auth()->user()->universities;
+            $universityName = $universities->isNotEmpty()
+                ? $universities->first()->nama_kampus
+                : 'No University Assigned';
+        @endphp
+
+        <div class="welcome">
+            Selamat datang, {{ Auth::user()->name }}<br>
+            Unit Sarpras PT {{ $universityName }}<br>
+        </div>
+        <div class="menu-item" data-toggle="submenu">Manajemen Aset</div>
+        <div class="submenu" style="display:none;">
+            <a href="{{ route('manajemen_aset.prasarana') }}" class="menu-item">Prasarana</a>
+            <a href="{{ route('manajemen_aset.sarana') }}" class="menu-item">Sarana</a>
+            <a href="" class="menu-item">Inventaris</a>
+        </div>
+
+    {{-- sarpras unit pusat --}}
     @elseif(auth()->check() && auth()->user()->role === '1')
-    <div class="welcome">
-        Selamat datang, {{ Auth::user()->name }}<br>
-        Unit Sarpras Pusat<br>
-    </div>
-    <div class="menu-item" data-toggle="submenu">Sumber Perolehan Aset</div>
-    <div class="submenu">
-        <a href="" class="menu-item">Pendanaan</a>
-        <a href="" class="menu-item">Data Paket</a>
-    </div>
-    <div class="menu-item" data-toggle="submenu">Manajemen Aset</div>
-    <div class="submenu">
-        <a href="{{ route('manajemen_aset.prasarana') }}" class="menu-item">Prasarana</a>
-        <a href="{{ route('manajemen_aset.sarana') }}" class="menu-item">Sarana</a>
-        <a href="" class="menu-item">Inventaris</a>
-    </div>
+        <div class="welcome">
+            Selamat datang, {{ Auth::user()->name }}<br>
+            Unit Sarpras Pusat<br>
+        </div>
+        <div class="menu-item" data-toggle="submenu">Sumber Perolehan Aset</div>
+        <div class="submenu" style="display:none;">
+            <a href="" class="menu-item">Pendanaan</a>
+            <a href="" class="menu-item">Data Paket</a>
+        </div>
+        <div class="menu-item" data-toggle="submenu">Manajemen Aset</div>
+        <div class="submenu" style="display:none;">
+            <a href="{{ route('manajemen_aset.prasarana') }}" class="menu-item">Prasarana</a>
+            <a href="{{ route('manajemen_aset.sarana') }}" class="menu-item">Sarana</a>
+            <a href="" class="menu-item">Inventaris</a>
+        </div>
     @endif
 </div>
 
-
-<!-- <script>
+<script>
     document.addEventListener('DOMContentLoaded', function() {
         var menuItems = document.querySelectorAll('[data-toggle="submenu"]'); // Select all toggles
         menuItems.forEach(function(menu) {
@@ -50,15 +53,6 @@
                     submenu.style.display = 'none';
                 }
             });
-        });
-    });
-</script> -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('.menu-item[data-toggle="submenu"]').click(function() {
-            $(this).toggleClass('active');
-            $(this).next('.submenu').slideToggle();
         });
     });
 </script>
