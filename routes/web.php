791a7c2343bf68fdfb625328182da17ebae8ef78
Fikri Maulana <maulanafikri455@gmail.com>
2024-05-15 18:49:50 +0700

    tambah fitur sidebar

--- routes/web.php
+++ routes/web.php
@@ -34,17 +34,9 @@ Route::post('manajemen_aset/sarana', [SarprasManajemenAset::class, 'tambah_saran
      ->name('manajemen_aset.sarana.create');
 Route::get('manajemen_aset/sarana', [SarprasManajemenAset::class, 'index_sarana'])
      ->name('manajemen_aset.sarana');
-Route::delete('manajemen_aset/sarana/delete/{id}', [SarprasManajemenAset::class, 'delete_sarana'])
-     ->name('manajemen_aset.sarana.delete');
-Route::get('manajemen_aset/sarana/{id}', [SarprasManajemenAset::class, 'get_data_ruangan'])
-     ->name('manajemen_aset.sarana.getData');
 
-Route::get('manajemen_aset/inventaris', [SarprasManajemenAset::class, 'index_inventaris'])
-     ->name('manajemen_aset.inventaris');
-Route::get('manajemen_aset/inventaris/{id_bangunan}', [SarprasManajemenAset::class, 'getRuangan'])
-     ->name('manajemen_aset.inventaris.getRuangan');
-Route::post('manajemen_aset/inventaris', [SarprasManajemenAset::class, 'tambah_pemetaan_dosen'])
-     ->name('manajemen_aset.inventaris.tambah_pemetaan_dosen');
+
+
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.post');
